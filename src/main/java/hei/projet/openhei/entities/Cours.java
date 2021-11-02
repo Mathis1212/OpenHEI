9babe8fe85b4be4b48f6a1d62e080de038784a15
Mathis Desruelles <mathis.desruelles@student.junia.com>
2021-11-02 17:25:03 +0100

    ajout des classes : Cours, Matiere, CoursDaoImpl, DataSourceProvider

--- src/main/java/hei/projet/openhei/entities/Cours.java
+++ src/main/java/hei/projet/openhei/entities/Cours.java
@@ -1,4 +1,31 @@
 package hei.projet.openhei.entities;
 
 public class Cours {
+    private Integer id;
+    private String nomCours;
+    private Matiere matiere;
+
+    public Cours(Integer id, String nomCours, Matiere matiere){
+        this.id=id;
+        this.nomCours=nomCours;
+        this.matiere=matiere;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getnomCours(){
+        return nomCours;
+    }
+    public void setNomCours(){
+        this.nomCours=nomCours;
+    }
+    public Matiere getMatiere(){
+        return matiere;
+    }
+    public void setMatiere(){
+        this.matiere=matiere;
+    }
 }
--- src/main/java/hei/projet/openhei/entities/User.java
+++ src/main/java/hei/projet/openhei/entities/User.java
@@ -1,4 +1,9 @@
 package hei.projet.openhei.entities;
 
 public class User {
+    private Integer id;
+    private String name;
+    private String login;
+    private String password;
+
 }
