ca622354554e1e9a7b9f449edee389e0aa870a5e
Florent VERCOURT <florent.vercourt@student.junia.com>
2021-11-03 11:42:47 +0100

    add a constructor to User.java

--- src/main/java/hei/projet/openhei/entities/User.java
+++ src/main/java/hei/projet/openhei/entities/User.java
@@ -2,8 +2,13 @@ package hei.projet.openhei.entities;
 
 public class User {
     private Integer id;
-    private String name;
+    private String pseudo;
     private String login;
     private String password;
+
+    User(Integer Id,String Pseudo,String Login,String Password){
+        this.id=Id;
+
+    }
 
 }
