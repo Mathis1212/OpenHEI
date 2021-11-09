6a32285a531c285053e38cc0496504c60afd24a5
Mathis Desruelles <mathis.desruelles@student.junia.com>
2021-11-09 09:55:22 +0100

    ConnectionServlet class edited

--- src/main/java/hei/projet/openhei/servlets/ConnectionServlet.java
+++ src/main/java/hei/projet/openhei/servlets/ConnectionServlet.java
@@ -1,2 +1,18 @@
-package hei.projet.openhei.servlets;public class ConnectionServlet {
+package hei.projet.openhei.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/connection")
+public class ConnectionServlet extends HttpServlet {
+    protected void doGet(final HttpServletRequest req,final HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
