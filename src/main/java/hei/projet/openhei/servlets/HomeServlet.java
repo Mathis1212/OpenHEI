c0fbfba155c3ca3a42fe96e08ac1f35eaac47604
bapt7448 <baptiste.pretet@student.junia.com>
2021-11-15 12:10:15 +0100

    homeServlet

--- src/main/java/hei/projet/openhei/servlets/HomeServlet.java
+++ src/main/java/hei/projet/openhei/servlets/HomeServlet.java
@@ -1,4 +1,14 @@
 package hei.projet.openhei.servlets;
 
-public class HomeServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/Acceuil")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
 }
