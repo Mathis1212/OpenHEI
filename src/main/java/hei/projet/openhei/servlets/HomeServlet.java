c722bbab8a0898939401d57fd7fe4d8b9a5cd341
bapt7448 <baptiste.pretet@student.junia.com>
2021-11-15 21:05:39 +0100

    HomeServlet

--- src/main/java/hei/projet/openhei/servlets/HomeServlet.java
+++ src/main/java/hei/projet/openhei/servlets/HomeServlet.java
@@ -1,5 +1,8 @@
 package hei.projet.openhei.servlets;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,8 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet("/Acceuil")
-public class HomeServlet extends HttpServlet {
+public class HomeServlet extends GenericServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
+        templateEngine.process("index", context, resp.getWriter());
     }
 }
