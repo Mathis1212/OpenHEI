48571ed810652754ee5ef8cd99cc35362c87c53b
Mathis Desruelles <mathis.desruelles@student.junia.com>
2021-12-05 19:26:25 +0100

    trying to implant delete button

--- src/main/java/hei/projet/openhei/servlets/ThemeServlet.java
+++ src/main/java/hei/projet/openhei/servlets/ThemeServlet.java
@@ -26,13 +26,14 @@ public class ThemeServlet extends GenericServlet{
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("matiere", MatiereService.getInstance().AssociationMatCour());
         TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
-        Boolean status = (Boolean) req.getSession().getAttribute("Status");
+        Boolean status = (Boolean) req.getSession().getAttribute("Admin");
         if ((status!=null)&&status){
-            resp.sendRedirect("ThemesAdmin");
+            resp.sendRedirect("/admin/ThemesAdmin");
+        }else {
+            String pseudo = (String) req.getSession().getAttribute("Pseudo");
+            context.setVariable("Connected", pseudo);
+            templateEngine.process("themes", context, resp.getWriter());
         }
-        String pseudo = (String) req.getSession().getAttribute("Pseudo");
-        context.setVariable("Connected", pseudo);
-        templateEngine.process("themes", context, resp.getWriter());
     }
 
     @Override
--- src/main/java/hei/projet/openhei/servlets/ThemesAdminServlet.java
+++ src/main/java/hei/projet/openhei/servlets/ThemesAdminServlet.java
@@ -1,5 +1,6 @@
 package hei.projet.openhei.servlets;
 
+import hei.projet.openhei.service.MatiereService;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -16,11 +17,10 @@ public class ThemesAdminServlet extends GenericServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("matiere", MatiereService.getInstance().AssociationMatCour());
         TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
-
         String pseudo = (String) req.getSession().getAttribute("Pseudo");
         context.setVariable("Pseudo", pseudo);
-
         templateEngine.process("themes_admin", context, resp.getWriter());
     }
 }
