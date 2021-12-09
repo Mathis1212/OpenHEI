e9d5092083dbbf76e5e4e5415ea3b7cf951e5096
Mathis <mathis.desruelles@student.junia.com>
2021-12-09 19:05:47 +0100

    Commentaires dans Dao

--- src/main/java/hei/projet/openhei/dao/CoursDao.java
+++ src/main/java/hei/projet/openhei/dao/CoursDao.java
@@ -7,17 +7,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+
+//Interface CoursDao
 public interface CoursDao {
-    public Cours createCoursFromResultSet(ResultSet resultSelect) throws SQLException; // corriger la creation d'instance dans le DaoImpl
 
-    public List<Cours> ListCour();
+    //Méthode de création d'un objet cours à la récupération des informations de la BDD
+    public Cours createCoursFromResultSet(ResultSet resultSelect) throws SQLException;
 
+    //Getters
     public String getNom(Integer id);
-
     public Matiere getMatiere(Integer id);
+
+    //Méthode d'ajout, d'update, et de suppression des cours dans la BDD
     public void addCour(Cours cours) throws SQLException;
     public Integer deleteCoursFromDB(String url_cours);
     public Integer updateCoursFromDB(String urlcoursToUpdate, String nom_cours, String url_cours);
+
+    //Méthode qui permet de vérifier si un cours éxiste dans la BDD grâce à son url (contrainte unique en BDD)
     public boolean ExistCours(String url_cours) throws SQLException;
 
+
+    public List<Cours> ListCour();
 }
